f1f03f94d0ced8675b231ebe95c02a7cf3c20b85
kriti-rai <kriti.rai@gmail.com>
2020-08-13 15:59:54 -0700

    Practice test

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,34 @@
-// Code Product Component Here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Product extends Component {
+    render() {
+      return(
+        <div>
+          <h2>{ this.props.name }</h2>
+          <p>{ this.props.producer }</p>
+          <p>{ this.props.color }</p>
+          <p>{ this.props.weight }</p>
+        </div>
+      )
+    }
+};
+
+let weightValidation = (props, propName) => {
+    let value = props[propName];
+    return (!((typeof value == 'number') && (value >= 80)  && (value <= 300))) ? 'Invalid': null;
+}
+
+const colors = ["white", "eggshell-white", "salmon"]
+
+Product.propTypes = {
+	name: PropTypes.string.isRequired,
+	producer: PropTypes.string,
+	hasWatermark: PropTypes.bool,
+	color: PropTypes.oneOf(colors).isRequired,
+	weight: weightValidation
+}
+
+Product.defaultProps = {
+  hasWatermark: false
+}
